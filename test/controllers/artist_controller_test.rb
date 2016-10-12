87da3978943b80132cdba343f686975a84905f72
Erin Bond <erin@erinebond.com>
2016-10-12 10:41:36 -0700

    Controller tests

--- test/controllers/artist_controller_test.rb
+++ test/controllers/artist_controller_test.rb
@@ -1,14 +1,17 @@
 require 'test_helper'
 
 class ArtistControllerTest < ActionController::TestCase
-  test "should get index" do
+
+  test "should get index" do #pass
     get :index
-    assert_response :success
+    assert_response :success #does it have somewhere to go
+    assert_template :index #is it going to the right place
   end
 
   test "should get show" do
     get :show, {id: 1 }
     assert_response :success
+    assert_template :show
   end
 
   test "should get new" do
@@ -17,22 +20,23 @@ class ArtistControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
-    get :edit
+    get :edit,  {id: 1 }
+    assert_response :success
+    assert_template :edit
+  end
+
+  test "should be able to create an artist" do
+    post :create
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
+  test "should be able to update an artist" do
+    patch :update,  {id: 1 }
     assert_response :success
   end
 
-  test "should get update" do
-    get :update
-    assert_response :success
-  end
-
-  test "should get delete" do
-    get :delete
+  test "should be able to delete an artist" do
+    delete :destroy,  {id: 1 }
     assert_response :success
   end
 
