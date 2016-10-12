bb9fbd1fd5d6de02ba519c34075eb9df3fe492a3
Erin Bond <erin@erinebond.com>
2016-10-12 10:22:55 -0700

    Tests for models

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -4,4 +4,15 @@ class AlbumTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "An Album must have a name and artist" do
+    assert albums(:slippery_when_wet).valid?
+    #Remove the name of the album so we can make sure it's invalid
+    albums(:slippery_when_wet).name = nil #local to this test only
+    assert_not albums(:slippery_when_wet).valid?
+  end
+
+    test "Slippery When Wet is by Bonjovi" do
+    assert_equal albums(:slippery_when_wet).artist, artists(:bonjovi)
+  end
 end
--- test/models/artist_test.rb
+++ test/models/artist_test.rb
@@ -9,9 +9,18 @@ class ArtistTest < ActiveSupport::TestCase
     red_hot_chilli_peppers = Artist.new
 
     assert_not red_hot_chilli_peppers.valid? "An Artist without a name is invalid."
-    red_hot_chilli_peppers.name = "Konye"
+    red_hot_chilli_peppers.name = "Kanye"
     assert red_hot_chilli_peppers.valid?  "Somehow Kanye is not a valid name."
   end
 
+  test "Retrieves the correct number of albums" do
+    assert_equal artists(:bonjovi).albums.length, 2
+    assert_equal artists(:beyonce).albums.length, 0
+  end
 
+  test "Retrieves the correct albums associated with an artist" do
+    assert artists(:bonjovi).albums.include?(albums(:slippery_when_wet))
+
+    assert_includes artists(:bonjovi).albums, albums(:slippery_when_wet)
+  end
 end
